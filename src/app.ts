49c87277c894029dbe40f14745080bd00f1929f0
deiviny <deiviny62@gmail.com>
2022-09-15 13:30:45 -0300

    Atulizaçao biblioteca

--- src/app.ts
+++ src/app.ts
@@ -8,18 +8,37 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.get('/newsession',(req: Request, res: Response) => {  
+    const { session } = req.body;  
+    sender.setNameSession = session    
+    
+    try {              
+        sender.newsession().then((dados) => {
+            return res.send({ 
+                qr_code: sender.qrCode,
+                connected: sender.isConnected,
+                name_session: sender.nameSession
+            })
+        });        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: "error", message: error });
+    }    
+})
+
 app.get('/status', (req: Request, res: Response) => {    
+    const { session } = req.body;
     return res.send({ 
         qr_code: sender.qrCode,
         connected: sender.isConnected
     })
 })
 
-app.post('/send', async (req: Request, res: Response) => {
-    const { number, message } = req.body;
+app.get('/send', async (req: Request, res: Response) => {
+    const { number, message, session } = req.body;
     try {
-        await sender.sendText(number, message);
-        return res.status(200).json()
+        await sender.sendText(number, message, session);
+        return res.status(200).json({ status: "success", message: "Mensagem enviada com sucesso" })
     } catch (error) {
         console.error(error);
         res.status(500).json({ status: "error", message: error });
--- src/sender.ts
+++ src/sender.ts
@@ -1,65 +1,102 @@
-import parsePhoneNumber, {isValidPhoneNumber} from "libphonenumber-js";
+import parsePhoneNumber, { isValidPhoneNumber } from "libphonenumber-js";
 import { create, Whatsapp, Message, SocketState } from "venom-bot"
 
 export type QRCode = {
     base64Qr: string
     attempt: number
 }
+
+export type SessionQr = {
+    [key: string]: QRCode
+}
+
+export type ClientSession = {
+    [key: string]: Whatsapp
+}
+
 class Sender {
-    private client: Whatsapp
+    private client: Whatsapp;
     private connected: boolean = false;
     private qr: QRCode;
-
+    private sessions: SessionQr;
+    nameSession: string = "first-session";
+    
     get isConnected(): boolean {
         return this.connected;
+    }
+
+    set setNameSession(session: string) {
+        this.nameSession = session;
+    }
+
+    get getNameSession(): string {
+        return this.nameSession
     }
 
     get qrCode(): QRCode {
         return this.qr
     }
 
-    constructor() {
-        this.initialize();
+    get sessionQr(): SessionQr {
+        return this.sessions
     }
 
-    async sendText(to: string, body: string) {
+    constructor() {
+        this.initialize()
+    }
 
-        if(!isValidPhoneNumber(to,"BR")){
+    async sendText(to: string, body: string, sessao: string) {
+        
+        if (!isValidPhoneNumber(to, "BR")) {
             throw new Error("Thisnumber is not valid")
         }
 
         let phoneNumber = parsePhoneNumber(to, "BR")?.format("E.164").replace("+", "") as string
 
-        phoneNumber = phoneNumber.includes("@c.us") 
-        ? phoneNumber 
-        : `${phoneNumber}@c.us`
-
-        console.log("phoneNumber", phoneNumber)
-
+        phoneNumber = phoneNumber.includes("@c.us")
+            ? phoneNumber
+            : `${phoneNumber}@c.us`        
         await this.client.sendText(phoneNumber, body)
     }
 
     private initialize() {
-
-        const qr = (base64Qr: string, asciiQr: string, attempt: number) => { 
-            this.qr = { base64Qr, attempt }
+        const sessionFirst = this.nameSession
+        const start = (client: Whatsapp) => {
+            this.client = client;
+            client.onStateChange((state) => {
+                this.connected = state === SocketState.CONNECTED
+            })
         }
+
+        create({
+            session: sessionFirst, //name of session
+            multidevice: true
+        })
+        .then((client) => start(client))
+        .catch((error) => console.error(error));
+    }
+
+    async newsession() {
 
         const status = (statusSession: string) => {
             this.connected = ["isLogged", "qrReadSuccess", "chatsAvailable"].includes(
                 statusSession
             )
         }
-
         const start = (client: Whatsapp) => {
             this.client = client;
-
             client.onStateChange((state) => {
                 this.connected = state === SocketState.CONNECTED
             })
         }
-
-        create("ws-sender-dev", qr, status)
+        
+        create(
+            this.nameSession,
+            (base64Qr: string, asciiQr: string, attempt: number) => {
+                this.qr = { base64Qr, attempt }
+                this.sessions[this.nameSession] = { base64Qr, attempt }
+            }
+        )
             .then((client) => start(client))
             .catch((error) => console.error(error));
     }
